3f34d23e0ed15582a521e38955a5ad9277bf0f99
Mahiro397 <21im0198@i-u.ac.jp>
2024-12-01 21:49:41 +0900

    認証機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import {withAuthenticator} from '@aws-amplify/ui-react'
+import "@aws-amplify/ui-react/styles.css"
+import awsExports from "./aws-exports"
+import { QRCodeSVG } from 'qrcode.react';
 
-function App() {
+
+Amplify.configure(awsExports);
+
+interface AppProps {
+  signOut: () => void; // signOut は関数
+  user: {
+    username: string; // user オブジェクトの型を定義
+  } | null; // user が null の場合も想定
+}
+function App({signOut,user}:AppProps) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
               こんにちは
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {user?(
+          <>
+          <h3>私は権限を持っています：{user.username}</h3>
+          <QRCodeSVG  value={user.username} />
+          <button onClick={signOut}>サインアウト </button>
+          </>
+        ):<h3>私は権限を持っていません。</h3>}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
