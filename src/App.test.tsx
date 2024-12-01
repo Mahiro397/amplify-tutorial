5b67da5689c2a768cf53cfcc4eac33cf3091a453
Mahiro397 <21im0198@i-u.ac.jp>
2024-12-02 01:28:24 +0900

    認証機能追加のエラー解消

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App signOut={() => {}} user={{ username: 'testUser' }} />);
-  const linkElement = screen.getByText(/こんにちは/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Amplify } from 'aws-amplify';
-import {withAuthenticator} from '@aws-amplify/ui-react'
+import { withAuthenticator} from '@aws-amplify/ui-react'
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css"
 import awsExports from "./aws-exports"
 import { QRCodeSVG } from 'qrcode.react';
@@ -10,29 +11,30 @@ import { QRCodeSVG } from 'qrcode.react';
 
 Amplify.configure(awsExports);
 
-interface AppProps {
-  signOut: () => void; // signOut は関数
-  user: {
-    username: string; // user オブジェクトの型を定義
-  } | null; // user が null の場合も想定
-}
-function App({signOut,user}:AppProps) {
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-              こんにちは
-        </p>
-        {user?(
-          <>
-          <h3>私は権限を持っています：{user.username}</h3>
-          <QRCodeSVG  value={user.username} />
-          <button onClick={signOut}>サインアウト </button>
-          </>
-        ):<h3>私は権限を持っていません。</h3>}
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+         <div className="App">
+         <header className="App-header">
+           <p>
+                 こんにちは
+           </p>
+           {user?(
+             <>
+             <h3>私は権限を持っています：{user.username}</h3>
+             <QRCodeSVG  value={user.username} />
+             <button onClick={signOut}>サインアウト </button>
+             </>
+           ):<h3>私は権限を持っていません。</h3>}
+         </header>
+       </div>
+      )}
+    </Authenticator>
   );
+   
+
 }
 
 export default withAuthenticator(App);
